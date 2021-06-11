b6d2075b442c0ab11456a7f755fe58eb649974b7
김예림 <kiel0103@naver.com>
2021-06-11 21:59:30 +0900

    feat: 단어 한개 뜻 찾기 interactive shell 구현

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 import vocabook.Koreans;
 import vocabook.VocaBook;
 import vocabook.VocaBookFactory;
@@ -7,15 +8,35 @@ import vocabook.HashMapPutDuplicateStrategy;
 public class Main {
 
     private static final String VOCA_FILE_NAME = "C:/Users/LG_PC/Desktop/정현 자바과제2 (1)/voca1800.txt";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         try {
             VocaBook vocaBook = VocaBookFactory.createFromVocaDataFile(VOCA_FILE_NAME,
                 HashMapPutDuplicateStrategy.USE_ORIGINAL);
 
-            Koreans koreans = vocaBook.searchKoreanMeaning("intellect");
-            System.out.println(koreans.toString());
+            System.out.println("원하는 기능을 선택하세요.\n"
+                + " 1 : 영어 단어뜻 검색");
+            System.out.print(">> ");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if (input == 1) {
+                search(vocaBook);
+            }
         } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void search(VocaBook vocaBook) {
+        System.out.println("찾을 영단어를 입력하세요 : ");
+        String input = scanner.nextLine();
+
+        try {
+            Koreans koreans = vocaBook.searchKoreanMeaning(input);
+            System.out.println(koreans.toString());
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
