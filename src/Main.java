0ed32f9f42ab6baffbdce699e8d1a2279ac54a28
김예림 <kiel0103@naver.com>
2021-06-12 01:37:50 +0900

    feat: 새 단어 등록 기능 및 반복 쉘 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import vocabook.HashMapPutDuplicateStrategy;
 import vocabook.Koreans;
 import vocabook.VocaBook;
@@ -7,10 +10,11 @@ import vocabook.VocaBookFactory;
 
 public class Main {
 
-    private static final String VOCA_FILE_NAME = "C:/Users/LG_PC/Desktop/정현 자바과제2 (1)/voca1800.txt";
     private static final int SEARCH = 1;
     private static final int PRINT_ALL = 2;
+    private static final int ADD_NEW_WORD = 3;
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String VOCA_FILE_NAME = "C:/Users/LG_PC/Desktop/정현 자바과제2 (1)/voca1800.txt";
 
     private static VocaBook vocaBook;
 
@@ -18,15 +22,20 @@ public class Main {
         try {
             vocaBook = VocaBookFactory.createFromVocaDataFile(VOCA_FILE_NAME,
                 HashMapPutDuplicateStrategy.USE_ORIGINAL);
+            int command ;
 
-            System.out.println("원하는 기능을 선택하세요.\n"
-                + " 1 : 영어 단어뜻 검색\n"
-                + " 2 : 전체 단어 보기");
-            System.out.print(">> ");
-            int input = Integer.parseInt(scanner.nextLine());
-
-            execute(input);
-        } catch (IOException e) {
+            do {
+                System.out.print("원하는 기능을 선택하세요.\n"
+                    + " 1 : 영어 단어뜻 검색\n"
+                    + " 2 : 전체 단어 보기\n"
+                    + " 3 : 영어 단어 추가\n"
+                    + " 0 : 종료\n");
+                System.out.print(">> ");
+                command = Integer.parseInt(scanner.nextLine());
+                execute(command);
+                System.out.println();
+            } while (command != 0);
+        } catch (IOException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -37,6 +46,12 @@ public class Main {
         }
         else if (commend == PRINT_ALL) {
             printAll();
+        }
+        else if (commend == ADD_NEW_WORD) {
+            addNewWord();
+        }
+        else {
+            throw new IllegalArgumentException("존재하지 않는 명령입니다.");
         }
     }
 
@@ -54,5 +69,22 @@ public class Main {
 
     private static void printAll() {
         System.out.println(vocaBook.toStringAll());
+    }
+
+    private static void addNewWord() {
+        System.out.println("추가할 영어 단어를 입력하세요.");
+        System.out.print(">> ");
+        String english = scanner.nextLine();
+
+        System.out.println("한국어 뜻을 입력하세요.");
+        System.out.println(">> ");
+        String[] meaningsInput = scanner.nextLine().split(",");
+
+        List<String> meanings = Arrays.stream(meaningsInput)
+            .map(String::trim)
+            .collect(Collectors.toList());
+
+        vocaBook.addWord(english, meanings);
+        System.out.println("단어가 추가되었습니다.");
     }
 }
